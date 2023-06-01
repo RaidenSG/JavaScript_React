057ef26d44ad9cede080e299795789084eaa3dcb
Kaushal <kaushalgg88@gmail.com>
2023-06-01 19:23:53 +0545

    objects

---
+++ Fundamentals/object/customMethod.js
@@ -0,0 +1,9 @@
+
+// let person1 = {
+//     firstName: "Shyam",
+//     lastName: "Khadka",
+//     // greet: () => console.log(`Hi my name is ${person1.firstName} ${person1.lastName}.`)
+
+//     greet: function(){console.log(`Hi my name is ${this.firstName} ${this.lastName}.`)}
+// }
+// person1.greet()
