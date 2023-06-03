8978f3838ab53dcf114d2c35b655e47f9378a6fe
Kaushal <kaushalgg88@gmail.com>
2023-06-03 20:35:15 +0545

    filter_array

--- Fundamentals/array/filter.js
+++ Fundamentals/array/filter.js
@@ -5,7 +5,13 @@ let numArr = [2,5,8,23,4,1,234,9]
 // let filterArr = numArr.filter(e => e%2!==0)
 // console.log(filterArr)
 
-let basicSalary = 15000;
-let salariesCom1 = [7600, 15000, 45000, 12000, 100000, 76000,1200000,43000, 3000, 5800, 35000];
-let filterSalary = salariesCom1.filter(e => e<basicSalary)
-console.log(`${filterSalary.length} people in this community is getting a below average salary.`)
+// let basicSalary = 15000;
+// let salariesCom1 = [7600, 15000, 45000, 12000, 100000, 76000,1200000,43000, 3000, 5800, 35000];
+// let filterSalary = salariesCom1.filter(e => e<basicSalary)
+// console.log(`${filterSalary.length} people in this community is getting a below average salary.`)
+
+function demo(){
+    arr = [1,2,3,4,5,6,7,8,9]
+    console.log(arr.filter(e => e<6))
+}
+demo()
