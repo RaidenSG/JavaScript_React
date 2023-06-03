62c64b5ad42979901ce88f4361f3bce1e235fcfe
Kaushal <kaushalgg88@gmail.com>
2023-06-03 20:36:25 +0545

    bank_Account

--- Questions/object/bankAcc.js
+++ Questions/object/bankAcc.js
@@ -2,17 +2,17 @@ const bankAcc = {
     firstName: "Ram",
     lastName: "Thapa",
     accountNo: 123456789,
-    currentBalance: 100000,
+    currentBalance: 10000,
     transaction : [],
-    fundDeposit: function(amount) {console.log(`${bankAcc.firstName} ${bankAcc.lastName}, ${amount} has been deposited in your Acc No. ${bankAcc.accountNo}and your current total is ${bankAcc.currentBalance+=amount}`)
-        this.transaction.push(this.amount)},  
+    fundDeposit: function(amount) {console.log(`${bankAcc.firstName} ${bankAcc.lastName}, ${amount} has been deposited in your Acc No. ${bankAcc.accountNo} and your current total is ${bankAcc.currentBalance+=amount}`)
+        this.transaction.push(amount)},  
 
     withDraw: function(amount){
-        if(this.currentBalance <= amount){
+        if(this.currentBalance < amount){
             console.log(`Dear ${this.firstName} ${this.lastName} you do not have sufficient balance in your bank account.`)
         }else{
             console.log(`You have withdraw Rs${amount}. Your current balance is ${bankAcc.currentBalance-=amount}`)
-            this.transaction.push(this.amount)
+            this.transaction.push(amount)
         }
     },
 
@@ -20,8 +20,11 @@ const bankAcc = {
 }
 
 
-bankAcc.fundDeposit(10000)
-bankAcc.withDraw(10000)
+bankAcc.fundDeposit(9000)
+bankAcc.withDraw(6000)
+bankAcc.fundDeposit(3000)
+bankAcc.withDraw(7000)
+bankAcc.withDraw(9000)
 console.log(bankAcc.currentBalance)
 
 console.log(bankAcc.transaction)
