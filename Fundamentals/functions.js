0aa97af58c58447dd63262dde2fd30a44899f881
Kaushal <kaushalgg88@gmail.com>
2023-06-03 20:20:34 +0545

    functions

--- Fundamentals/functions.js
+++ Fundamentals/functions.js
@@ -6,7 +6,14 @@
 // const currentAge = callMe(20)
 // console.log(currentAge)
 
-callMe = age => 2023 - age
-const age = 2023 - age
-const retirement = 65 - age
-console.log
+// callMe = age => 2023 - age
+// const age = 2023 - age
+// const retirement = 65 - age
+// console.log
+
+function demo(){
+    // console.log('First')
+     return console.log('Welcome')
+}
+const call = demo()
+console.log(call)
