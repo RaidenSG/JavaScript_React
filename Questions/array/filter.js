3b539db2adeab9e768398945e2a562de8e6b621b
Kaushal <kaushalgg88@gmail.com>
2023-06-21 22:24:32 +0545

    filter_Method

--- Questions/array/filter.js
+++ Questions/array/filter.js
@@ -1,15 +1,20 @@
-let basicSalary = 15000;
-let salariesCom1 = [7600, 15000, 45000, 12000, 100000, 76000,1200000,43000, 3000, 5800, 35000];
-// let filterSalary = salariesCom1.filter(e => e<basicSalary)
-// console.log(`${filterSalary.length} people in this community is getting a below average salary.`)
+// let basicSalary = 15000;
+// let salariesCom1 = [7600, 15000, 45000, 12000, 100000, 76000,1200000,43000, 3000, 5800, 35000];
+// // let filterSalary = salariesCom1.filter(e => e<basicSalary)
+// // console.log(`${filterSalary.length} people in this community is getting a below average salary.`)
 
-//Create an another array that shows percentage difference of the filterSalary to be 15000
-//Eg: 7500 salary is lagging by 50% to be 15000,.......
+// //Create an another array that shows percentage difference of the filterSalary to be 15000
+// //Eg: 7500 salary is lagging by 50% to be 15000,.......
 
-// let showPer = filterSalary.map(e => `${e} salary is lagging by ${(e/basicSalary) *100}% to be ${basicSalary}`)
-// console.log(showPer)
+// // let showPer = filterSalary.map(e => `${e} salary is lagging by ${(e/basicSalary) *100}% to be ${basicSalary}`)
+// // console.log(showPer)
 
-//Method Chaining
+// //Method Chaining
 
-let filterSalary = salariesCom1.filter(e => e<basicSalary).map(e => `${e} salary is lagging by ${(e/basicSalary) *100}% to be ${basicSalary}`)
-console.log(filterSalary)
+// let filterSalary = salariesCom1.filter(e => e<basicSalary).map(e => `${e} salary is lagging by ${(e/basicSalary) *100}% to be ${basicSalary}`)
+// console.log(filterSalary)
+
+let arr1 = [1,2,3,4,5,6,7]
+let arr2 = arr1.filter(e=>e%2)
+console.log(arr1)
+console.log(arr2)
