8017ce2a07a160ab825b3e7e6110469cb6f24125
Kaushal <kaushalgg88@gmail.com>
2023-07-09 18:59:45 +0545

    About_Destructuring

--- Fundamentals/array/destructuring.js
+++ Fundamentals/array/destructuring.js
@@ -1,28 +1,41 @@
-const arr = [1,2,3,4]
-// const a = arr[0]
-// const b = arr[1]
-// const c = arr[2]
-// const d = arr[3]
-// console.log(a,b,c,d)
+// const arr = [1,2,3,4]
+// // const a = arr[0]
+// // const b = arr[1]
+// // const c = arr[2]
+// // const d = arr[3]
+// // console.log(a,b,c,d)
 
-const [w,x,y,z] = arr
-// console.log(w,x,y,z)
+// const [w,x,y,z] = arr
+// // console.log(w,x,y,z)
 
 const shop = {
     name: 'Kirana Shop',
     location: 'Pokhara, Kaski, Nepal',
     goods: ['Vegetables','Cusmetics','Fruits','Dairy','Organic'],
     price: ['cheap','moderate','expensive'],
-    pick: function(first,second){
+    pick: (first,second)=>{
         return [shop.goods[first],shop.price[second]]
     },
 };
-console.log(shop.pick(3,2))
+// console.log(shop.pick(3,2))
 
-// const [ele1,ele2] = shop.categories
-// console.log(ele1,ele2)
+let [a,,b] = shop.goods
+console.log(a,b)
 
-// [ele1,ele2] = [ele2,ele1];
-// console.log(ele1,ele2);
-// const [food,cost] = shop.order(3,2)
+
+
+// [a,b] = [b,a];
+// console.log(a,b);
 // console.log(food,cost)
+
+// const [food,cost] = shop.order(3,2)
+
+//nested destructuring
+// const nested = [2,4,[5,6]]
+// const [i,,[j,k]] = nested
+// console.log(i,j,k)
+
+//default value
+let default1 = [1,2]
+let [x=1,y=1,z=1] = default1
+console.log(x,y,z)
